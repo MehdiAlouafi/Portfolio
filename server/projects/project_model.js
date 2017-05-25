962a52a9232f46f1b8d061ffbf443d3d7b307e3c
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-05-25 03:51:52 +0200

    définition du project model

---
+++ server/projects/project_model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+
+const projectSchema = new mongoose.Schema({
+	title: { type: String, required: true },
+	published: { type: Boolean, default: false },
+	addedAt: { type: String, default: moment().format('YYYY-MM-DD')},
+	content: { type: Array, required: true }
+});
+
+module.exports = mongoose.model('Project', projectSchema);
