172732f9875967e5dd040003e56f81e0c415fb05
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-05-25 03:51:08 +0200

    CRUD Projects
    Création des routes GET, POST, PUT, DELETE corresspondant aux
    pattern classique des apps CRUD.
    
    A noter ici que j'utiliser un middleware provenant d'un paquet
    body-parser qui comme son nom l'indique 'parse' le body de la request et
    nous le rend accessible via la propriété 'req.body'.
    
    Après le body Parser je fais appel aux methodes du controller Project
    corresspondant à chaque méthode http.

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,7 +1,23 @@
 const projectRouter = require('express').Router();
+const ProjectController = require('../projects/project_controller');
+const Project = require('../projects/project_model');
+const bodyParser = require('body-parser').urlencoded({ extended: true });
 
-
+/*
+	- 	exemple de requête et de récupération de l'id : Mon-Project-perso-OBJECTID
+		cela à pour but de bénéficier d'une meilleur indexation du contenu sur les
+		moteurs de recherches.
+	.get('/projects/:id', (req, res) => {
+		const params = req.params.id.split('-');
+		console.log(params);
+		const id = params[params.length - 1];
+		res.send(id);
+	})
+*/
 projectRouter
-	.get('/projects', (req, res) => res.send('Hello from /projects route'));
+	.get('/projects', ProjectController.getAll)
+	.post('/projects', bodyParser, ProjectController.createOne)
+	.put('/projects/:id', bodyParser, ProjectController.updateOne)
+	.delete('/projects/:id', bodyParser, ProjectController.deleteOne)
 
 module.exports = projectRouter;
