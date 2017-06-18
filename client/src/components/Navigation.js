37a744548b21b8e7d3a8116197b484de58df5f44
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-06-18 16:09:56 +0200

    Show /dashboard link if tokenADM exists

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 const Navigation = (props) => {
+    const showDashboardLink = localStorage.getItem('tokenADM') !== null;
     return (
         <nav>
+            {
+                showDashboardLink &&
+                <NavLink
+                    activeStyle={{ backgroundColor: '#FC153B'}}
+                    to='/dashboard'>Dashboard</NavLink>
+            }
             <NavLink
                 activeStyle={{ backgroundColor: '#FC153B'}}
                 to='/projets'>Projets</NavLink>
