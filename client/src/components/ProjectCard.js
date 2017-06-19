a9fe13aaa6659e449f0ede18dad4245e23199ecf
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-06-19 13:06:29 +0200

    Update ProjectCard
    - Nouvelle UI pour Admin

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Switch from 'rc-switch';
 
-const ProjectCard = ({ project }) => {
+const ProjectCard = ({ project, dashboard, togglePublish, deleteProject }) => {
+    if (dashboard) {
+        return (
+            <div className='project project__dashboard'>
+                <h2>{project.title}</h2>
+                <p>{project.addedAt}</p>
+                <Switch
+                    onClick={() => togglePublish(project)}
+                    checked={project.published} />
+                <button
+                    onClick={() => deleteProject(project._id)}
+                    type='button'>
+                    <img style={{width: 20}} src='./assets/images/trash.svg' alt='Une poubelle' />
+                </button>
+                <Link to={`/projects/${project._id}`}>Read</Link>
+            </div>
+        );
+    }
     return (
         <div className='project'>
-            <h1>{project.title}</h1>
+            <h2>{project.title}</h2>
             <p>{project.addedAt}</p>
             <Link to={`/projects/${project._id}`}>Read</Link>
         </div>
