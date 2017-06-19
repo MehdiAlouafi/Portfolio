8fc2ae4fd08db8ae1f017b325af4413ea84df0a9
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-06-19 17:19:13 +0200

    handleUpload Editor

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -12,6 +12,7 @@ class Editor extends React.Component {
         this.handleParagraphChange = this.handleParagraphChange.bind(this);
         this.handleTitleChange = this.handleTitleChange.bind(this);
         this.addParagraph = this.addParagraph.bind(this);
+        this.handleUpload = this.handleUpload.bind(this);
     }
     addParagraph({ keyCode }) {
         if (keyCode === 13) {
@@ -21,7 +22,7 @@ class Editor extends React.Component {
                     type: 'p',
                     text: ''
                 })
-            }))
+            }));
         }
     }
     handleParagraphChange(event, idx) {
@@ -36,7 +37,7 @@ class Editor extends React.Component {
         this.setState(prevState => ({
             ...prevState,
             title: value
-        }))
+        }));
     }
     componentDidMount() {
         document.addEventListener('keydown', this.addParagraph);
@@ -44,21 +45,35 @@ class Editor extends React.Component {
     componentWillUnmount() {
         document.removeEventListener('keydown', this.addParagraph);
     }
+    handleUpload(event) {
+        const form = new FormData();
+        const [ file ] = event.target.files;
+        form.append('image', file);
+        fetch(`http://localhost:8080/api/upload/${this.state.title}`, {
+            method: 'POST',
+            body: form
+        }).then(res => console.log(res)).catch(err => console.log(err));
+    }
     render() {
         return (
             <div>
-                <input
-                    type='text'
-                    value={this.state.title}
-                    onChange={this.handleTitleChange} />
-                { this.state.content.map((p, i) => {
-                    return (
-                        <input
-                            type='text'
-                            key={i}
-                            onChange={(e) => this.handleParagraphChange(e, i)}
-                            value={p.text} />)
-                })}
+                <div className='editor__content'>
+                    <input
+                        type='text'
+                        value={this.state.title}
+                        onChange={this.handleTitleChange} />
+                    { this.state.content.map((p, i) => {
+                        return (
+                            <input
+                                type='text'
+                                key={i}
+                                onChange={(e) => this.handleParagraphChange(e, i)}
+                                value={p.text} />
+                            )
+                    })}
+                    <label htmlFor="upload">YPLOAD</label>
+                    <input onChange={this.handleUpload} id='upload' type='file' accept='image/*'/>
+                </div>
             </div>
         );
     }
