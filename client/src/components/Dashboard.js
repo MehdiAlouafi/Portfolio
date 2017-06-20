6d624c321f6b1a8efc69f6752337c5143b0786d6
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-06-20 14:05:19 +0200

    update dashboard
    - handle errors

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Redirect, Link } from 'react-router-dom';
 
-import AddProject from './AddProject';
 import ProjectCard from './ProjectCard';
+
 
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            errMessage: '',
             projects: [],
             fetched: false,
             error: false,
@@ -76,14 +78,36 @@ class Dashboard extends React.Component {
             }
         };
         fetch('http://localhost:8080/api/projects', options)
-            .then(res => res.json())
+            .then(res => {
+                if (res.status !== 200) {
+                    throw new Error(res.statusText)
+                }
+                return res.json();
+            })
             .then(projects => this.setState({ projects, fetched: true }))
-            .catch(err => this.setState({ error: true }));
+            .catch(error => {
+                localStorage.removeItem('tokenADM');
+                this.setState({ fetched: true, error: true, errMessage: error.message})
+            });
         //}
     }
     render() {
+        const {
+            fetched,
+            error: hasError,
+            errMessage
+        } = this.state;
+
         if (this.state.fetched === false) return <p>Loading...</p>
-        if (this.state.error === true ) return null // PANIC
+        if (this.state.error === true ) {
+            return (
+                <Redirect to={{
+                    pathname: '/',
+                    state: { errMessage }
+                }}/>
+            );
+        }
+        const label = this.state.projects.length === 0 ? 'Pas encore de projets, addOne' : 'addOne';
         const projects = this.state.projects.map((project, i) => (
             <ProjectCard
                 dashboard
@@ -96,10 +120,9 @@ class Dashboard extends React.Component {
             <div>
                 <h1>Dashboard</h1>
                 { projects }
-                <AddProject
-                    newProject={() => this.props.history.push('/editor')}
-                    length={this.state.projects.length}
-                />
+                <Link to='/editor'>
+                    {label}
+                </Link>
             </div>
         );
     }
