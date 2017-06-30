097d0eddb5a12bcb1c6a5927110d09d37d39adc8
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-06-30 16:02:31 +0200

    about component

---
+++ client/src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+        <p className='kicker f2-ns f3-m f4'>
+            Mehdi AL OUAFI, originaire de Paris. Grâce à Simplon.co je me suis découvert une passion dans le développement web et mobile. <br />
+            Anciennement apprenant promo#4 chez Simplon, je suis aujourd'hui développeur web fullstack js chez SimplonProd.
+        </p>
+);
+
+export default About;
