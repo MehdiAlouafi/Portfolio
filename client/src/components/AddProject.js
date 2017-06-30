cce05118f1e58598c50ed547d514200a99b60846
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-06-30 16:02:18 +0200

    delete

--- client/src/components/AddProject.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const AddProject = ({ length, newProject }) => {
-    const label = length === 0 ? 'Pas encore de projets, addOne' : 'addOne';
-    return <button onClick={newProject} className='add_project'>{label}</button>
-}
-
-export default AddProject;
