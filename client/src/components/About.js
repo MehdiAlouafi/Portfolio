3bca1644ff0f8c7729a63fcbdf953a3723c79c2b
Mehdi Al ouafi <alouafi.mehdi@gmail.com>
2017-07-21 02:53:49 +0200

    update

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+const styles = {
+    textDecoration: 'underline'
+};
 const About = () => (
-        <p className='kicker f2-ns f3-m f4'>
-            Mehdi AL OUAFI, originaire de Paris. Grâce à Simplon.co je me suis découvert une passion dans le développement web et mobile. <br />
-            Anciennement apprenant promo#4 chez Simplon, je suis aujourd'hui développeur web fullstack js chez SimplonProd.
-        </p>
+        <div>
+            <p className='f3-ns f3-m f4'>
+                J'ai découvert le code pour la toute première fois à Simplon où j'y ai suivi une formation de développeur web durant 6 mois. <br />
+                J'ai ensuite été accepté en tant qu'alternant au sein de l'équipe de SimplonProd, le pôle d'innovation numérique de Simplon, avec qui je continue
+                aujourd'hui mon aventure.
+            </p>
+            <p className='f3-ns f3-m f4'>
+                Beaucoup de choses se sont enchaînées pour moi ces 2 dernières années, j'ai pu découvrir une passion.
+            </p>
+            <p className='f3-ns f3-m f4'>
+                Je code en Javascript : ReactJS, Redux, NodeJS, MeteorJS, Npm, Webpack, ...<br />
+                <Link style={styles} to='/projets'>Découvrez les  projets sur lesquels j'ai travaillé </Link>
+            </p>
+        </div>
 );
 
 export default About;
--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const NotFound = () => (
-    <p>Oops 404 Not Found</p>
+    <p>Oops 404 Not Found. <Link style={{ textDecoration: 'underline'}} to='/'>Retournez à la Page d'accueil</Link></p>
 );
 
 export default NotFound;
